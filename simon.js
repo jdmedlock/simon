f3132552332c86245e684fd10e27328a36627bc3
Jim Medlock <jdmedlock@gmail.com>
2016-12-23 06:41:42 -0600

    added barebones button handlers

--- simon.js
+++ simon.js
@@ -23,9 +23,39 @@
 $(document).ready(function() {
     console.clear();
 
-    // Create a button handler for the help dialog
-    $(".si-btn-red").click(function(event) {
+    // Create a button handler for the on/off button
+    $("#si-btn-onoff").click(function(event) {
+        console.log("Clicked on the on/off button");
+    });
+
+    // Create a button handler for the start button
+    $("#si-btn-start").click(function(event) {
+        console.log("Clicked on the start button");
+    });
+
+    // Create a button handler for the strict button
+    $("#si-btn-strict").click(function(event) {
+        console.log("Clicked on the strict button");
+    });
+
+    // Create a button handler for the red player response button
+    $("#si-btn-red").click(function(event) {
         console.log("Clicked on the red button");
+    });
+
+    // Create a button handler for the blue player response button
+    $("#si-btn-blue").click(function(event) {
+        console.log("Clicked on the blue button");
+    });
+
+    // Create a button handler for the green player response button
+    $("#si-btn-green").click(function(event) {
+        console.log("Clicked on the green button");
+    });
+
+    // Create a button handler for the yellow player response button
+    $("#si-btn-yellow").click(function(event) {
+        console.log("Clicked on the yellow button");
     });
 
 });
