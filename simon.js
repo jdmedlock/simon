7f262e75766918990bd659ddd2fd12a614328347
Jim Medlock <jdmedlock@gmail.com>
2016-12-23 13:29:54 -0600

    added onOffButton and startButton objects

--- simon.js
+++ simon.js
@@ -26,11 +26,13 @@ $(document).ready(function() {
     // Create a button handler for the on/off button
     $("#si-btn-onoff").click(function(event) {
         console.log("Clicked on the on/off button");
+        onOffButton.buttonPress(this);
     });
 
     // Create a button handler for the start button
     $("#si-btn-start").click(function(event) {
         console.log("Clicked on the start button");
+        startButton.buttonPress(this);
     });
 
     // Create a button handler for the strict button
@@ -61,5 +63,60 @@ $(document).ready(function() {
 });
 
 // -------------------------------------------------------------
-// User Interface functions
+// Object Definitions
 // -------------------------------------------------------------
+
+// On/Off Button Object
+const gameOn = true;
+const gameOff = false;
+
+let onOffButton = {
+   gameState: gameOff,
+   // Toggles the current game state. When the state changes from
+   // off to on the state of the Start and Strict buttons will be
+   // reset and the score will be set to '00'.
+   //
+   // Returns: N/a
+   buttonPress: (thisButton) => {
+      if (this.gameState == gameOff) {
+         this.gameState = gameOn;
+         $(thisButton).removeClass("si-btn-active");
+         $(thisButton).addClass("si-btn-active");
+         $(thisButton).text("On");
+      } else {
+         this.gameState = gameOff;
+         $(thisButton).removeClass("si-btn-active");
+         $(thisButton).text("Off");
+      }
+   },
+   // Returns: the current game state.
+   isGameOn: () => {
+      return this.gameState;
+   }
+};
+
+// Start Button Object
+const gameInprogress = true;
+const gameWaiting = false;
+
+let startButton = {
+   gameMode: gameWaiting,
+   // Starts and stops game play. When the state changes
+   // the score will be set to '00'.
+   //
+   // Returns: N/a
+   buttonPress: (thisButton) => {
+      if (onOffButton.isGameOn() == gameOff) {
+         if (this.gameMode == gameWaiting) {
+            this.gameMode = gameInprogress;
+            $(thisButton).removeClass("si-btn-active");
+            $(thisButton).addClass("si-btn-active");
+            $(thisButton).text("Stop");
+         } else {
+            this.gameMode = gameWaiting;
+            $(thisButton).removeClass("si-btn-active");
+            $(thisButton).text("Start");
+         }
+      }
+   }
+};
