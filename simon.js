0ab400a3e2021a5efdc36a819783e887d79ffca2
Jim Medlock <jdmedlock@gmail.com>
2016-12-25 08:12:40 -0600

    fleshing out gameEngine methods

--- simon.js
+++ simon.js
@@ -16,60 +16,60 @@
 //
 // Returns: N/a
 $(document).ready(function() {
-    console.clear();
-    onOffButton.setInitialState();
+   console.clear();
+   onOffButton.setInitialState();
 
-    // Create a button handler for the on/off button
-    $("#si-btn-onoff").click(function(event) {
-        console.log("Clicked on the on/off button");
-        onOffButton.buttonPress();
-    });
+   // Create a button handler for the on/off button
+   $("#si-btn-onoff").click(function(event) {
+      console.log("Clicked on the on/off button");
+      onOffButton.buttonPress();
+   });
 
-    // Create a button handler for the start button
-    $("#si-btn-start").click(function(event) {
-        console.log("Clicked on the start button");
-        startButton.buttonPress();
-    });
+   // Create a button handler for the start button
+   $("#si-btn-start").click(function(event) {
+      console.log("Clicked on the start button");
+      startButton.buttonPress();
+   });
 
-    // Create a button handler for the strict button
-    $("#si-btn-strict").click(function(event) {
-        console.log("Clicked on the strict button");
-        strictButton.buttonPress();
-    });
+   // Create a button handler for the strict button
+   $("#si-btn-strict").click(function(event) {
+      console.log("Clicked on the strict button");
+      strictButton.buttonPress();
+   });
 
-    // Create a button handler for the help dialog
-    $("#si-btn-help").click(function(event) {
-        $("#si-help-dialog").css("display", "block");
-    });
+   // Create a button handler for the help dialog
+   $("#si-btn-help").click(function(event) {
+      $("#si-help-dialog").css("display", "block");
+   });
 
-    // Create a button handler to close the help dialog
-    $(".si-dialog-close").click(function(event) {
-        $("#si-help-dialog").css("display", "none");
-    });
+   // Create a button handler to close the help dialog
+   $(".si-dialog-close").click(function(event) {
+      $("#si-help-dialog").css("display", "none");
+   });
 
-    // Create a button handler for the red player response button
-    $("#si-btn-red").click(function(event) {
-        console.log("Clicked on the red button");
-        responseButton.buttonPress(this);
-    });
+   // Create a button handler for the red player response button
+   $("#si-btn-red").click(function(event) {
+      console.log("Clicked on the red button");
+      responseButton.buttonPress(this);
+   });
 
-    // Create a button handler for the blue player response button
-    $("#si-btn-blue").click(function(event) {
-        console.log("Clicked on the blue button");
-        responseButton.buttonPress(this);
-    });
+   // Create a button handler for the blue player response button
+   $("#si-btn-blue").click(function(event) {
+      console.log("Clicked on the blue button");
+      responseButton.buttonPress(this);
+   });
 
-    // Create a button handler for the green player response button
-    $("#si-btn-green").click(function(event) {
-        console.log("Clicked on the green button");
-        responseButton.buttonPress(this);
-    });
+   // Create a button handler for the green player response button
+   $("#si-btn-green").click(function(event) {
+      console.log("Clicked on the green button");
+      responseButton.buttonPress(this);
+   });
 
-    // Create a button handler for the yellow player response button
-    $("#si-btn-yellow").click(function(event) {
-        console.log("Clicked on the yellow button");
-        responseButton.buttonPress(this);
-    });
+   // Create a button handler for the yellow player response button
+   $("#si-btn-yellow").click(function(event) {
+      console.log("Clicked on the yellow button");
+      responseButton.buttonPress(this);
+   });
 });
 
 // -------------------------------------------------------------
@@ -87,36 +87,36 @@ let onOffButton = {
    //
    // Returns: N/a
    setInitialState: () => {
-     this.gameState = gameOff;
-     $("#si-btn-onoff").removeClass("si-btn-active");
-     $("#si-btn-onoff").addClass("si-btn-inactive");
-     $("#si-btn-onoff").text("On");
-     startButton.stopGame();
-     strictButton.normalGame();
+      this.gameState = gameOff;
+      $("#si-btn-onoff").removeClass("si-btn-active");
+      $("#si-btn-onoff").addClass("si-btn-inactive");
+      $("#si-btn-onoff").text("On");
+      startButton.stopGame();
+      strictButton.normalGame();
    },
 
    // Turn the game off
    //
    // Returns: N/a
    turnOff: () => {
-     this.gameState = gameOff;
-     $("#si-btn-onoff").removeClass("si-btn-active");
-     $("#si-btn-onoff").addClass("si-btn-inactive");
-     $("#si-btn-onoff").text("On");
-     startButton.stopGame();
-     strictButton.normalGame();
+      this.gameState = gameOff;
+      $("#si-btn-onoff").removeClass("si-btn-active");
+      $("#si-btn-onoff").addClass("si-btn-inactive");
+      $("#si-btn-onoff").text("On");
+      startButton.stopGame();
+      strictButton.normalGame();
    },
 
    // Turn the game on
    //
    // Returns: N/a
    turnOn: () => {
-     this.gameState = gameOn;
-     $("#si-btn-onoff").removeClass("si-btn-inactive");
-     $("#si-btn-onoff").addClass("si-btn-active");
-     $("#si-btn-onoff").text("Off");
-     startButton.stopGame();
-     strictButton.normalGame();
+      this.gameState = gameOn;
+      $("#si-btn-onoff").removeClass("si-btn-inactive");
+      $("#si-btn-onoff").addClass("si-btn-active");
+      $("#si-btn-onoff").text("Off");
+      startButton.stopGame();
+      strictButton.normalGame();
    },
 
    // Toggles the current game state. When the state changes from
@@ -151,20 +151,20 @@ let startButton = {
    //
    // Returns: N/a
    newGame: () => {
-     this.gameMode = gameInprogress;
-     $("#si-btn-start").removeClass("si-btn-inactive");
-     $("#si-btn-start").addClass("si-btn-active");
-     $("#si-btn-start").text("Stop");
+      this.gameMode = gameInprogress;
+      $("#si-btn-start").removeClass("si-btn-inactive");
+      $("#si-btn-start").addClass("si-btn-active");
+      $("#si-btn-start").text("Stop");
    },
 
    // Stop the game
    //
    // Returns: N/a
    stopGame: () => {
-     this.gameMode = gameWaiting;
-     $("#si-btn-start").removeClass("si-btn-active");
-     $("#si-btn-start").addClass("si-btn-inactive");
-     $("#si-btn-start").text("Start");
+      this.gameMode = gameWaiting;
+      $("#si-btn-start").removeClass("si-btn-active");
+      $("#si-btn-start").addClass("si-btn-inactive");
+      $("#si-btn-start").text("Start");
    },
 
    // Starts and stops game play. When the state changes
@@ -200,18 +200,18 @@ let strictButton = {
    //
    // Returns: N/a
    strictGame: () => {
-     this.playMode = playStrict;
-     $("#si-btn-strict").removeClass("si-btn-inactive");
-     $("#si-btn-strict").addClass("si-btn-active");
+      this.playMode = playStrict;
+      $("#si-btn-strict").removeClass("si-btn-inactive");
+      $("#si-btn-strict").addClass("si-btn-active");
    },
 
    // Enable normal play mode
    //
    // Returns: N/a
    normalGame: () => {
-     this.playMode = playNormal;
-     $("#si-btn-strict").removeClass("si-btn-active");
-     $("#si-btn-strict").addClass("si-btn-inactive");
+      this.playMode = playNormal;
+      $("#si-btn-strict").removeClass("si-btn-active");
+      $("#si-btn-strict").addClass("si-btn-inactive");
    },
 
    // Toggles between normal and strict game play
@@ -240,30 +240,49 @@ let strictButton = {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Player Response Button Object
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const colorRed = 0;
-const colorBlue = 1;
-const colorGreen = 2;
-const colorYellow = 3;
+const colors = [{
+   name: "red",
+   number: 0
+}, {
+   name: "blue",
+   number: 1
+}, {
+   name: "green",
+   number: 2
+}, {
+   name: "yellow",
+   number: 3
+}];
 const responseButtonPrefix = "si-btn-";
 
 let responseButton = {
-   // Create a new player response series
+   // Add the button press to the current set of player responses.
    //
-   // Returns: N/a
-   newPlayerSeries: () => {
-     this.playerSeries = [];
-   },
-
-   // Add the button press to the current set of player responses
+   // Pre-conditions:
+   //    1. The game series contains at least one computer generated color.
+   //    2. The player series has been created and contains one entry less
+   //       than the game series.
    //
    // Returns: N/a
    buttonPress: (thisButton) => {
       if (onOffButton.isGameOn()) {
-        let buttonName = $(thisButton).attr("id");
-        let buttonColor = buttonName.slice(responseButtonPrefix.length);
-        console.log(buttonColor);
+         let buttonColorName = $(thisButton).attr("id")
+            .buttonName.slice(responseButtonPrefix.length);
+         let buttonColorNum = colors.find((color, index, array) => {
+            return color.number === buttonColorName;
+         });
+         this.playerSeries.push(buttonColorNum);
+         gameEngine.play(buttonColorNum);
       }
+   },
+
+   // Create a new player response series
+   //
+   // Returns: N/a
+   newPlayerSeries: () => {
+      this.playerSeries = [];
    }
+
 };
 
 //~~~~~~~~~~~~~~~~~~~
@@ -282,7 +301,7 @@ let gameEngine = {
    //              the series
    //          playerHasWon if the response matches after 20 consecutive matches
    //          playerResponseDiffers if it doesn't match.
-   checkPlayerResponse: (position, playerColor) => {
+   checkPlayerResponse: (position, playerColorNum) => {
       if (this.playsInSeries[position] === playerColor) {
          if (this.playsInSeries.length === playLimit) {
             return playerHasWon;
@@ -297,6 +316,9 @@ let gameEngine = {
    // Returns: N/a
    createNewSeries: () => {
       this.playsInSeries = [];
+      responseButton.newPlayerSeries();
+      this.generateNewColor();
+      this.replaySeries();
    },
 
    // Retrieve the most recent color generated by the computer in the current
@@ -307,7 +329,7 @@ let gameEngine = {
    //          empty.
    getLastInSeries: () => {
       if (this.playsInSeries.length > 0) {
-         return this.playsInSeries[length-1];
+         return this.playsInSeries[length - 1];
       }
       return null;
    },
@@ -322,6 +344,21 @@ let gameEngine = {
       let newColor = Math.floor(Math.random() * (max - min + 1)) + min;
       this.playsInSeries.push(newColor);
       return newColor;
+   },
+
+   // Play a turn by evaluating the most recent player response against the
+   // most recent computer generated color.
+   //
+   // Pre-conditions:
+   //    1. The player and game series arrays should contain the same number of
+   //       entries.
+   //
+   // Returns: N/a
+   play: (buttonColorNum) => {
+      let playStatus = this.checkPlayerResponse((responseButton.playerSeries.length - 1), buttonColorNum);
+
+
+      this.replaySeries();
    },
 
    // Replay the current series
